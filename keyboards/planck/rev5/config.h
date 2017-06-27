1cdf3e9d5e233d79f32ee3c66783f850c7ba77bf
Jack Humbert <jack.humb@gmail.com>
2017-06-26 22:24:30 -0400

    b5 audio

--- keyboards/planck/rev5/config.h
+++ keyboards/planck/rev5/config.h
@@ -13,7 +13,9 @@
 
 #undef BACKLIGHT_PIN
 
-#define BACKLIGHT_PIN A5
+#define NO_BACKLIGHT_CLOCK
+
+#define B5_AUDIO
 
 // This is a 7-bit address, that gets left-shifted and bit 0
 // set to 0 for write, 1 for read (as per I2C protocol)
--- keyboards/planck/rev5/rules.mk
+++ keyboards/planck/rev5/rules.mk
@@ -1,6 +1,7 @@
-MIDI_ENABLE = no
+MIDI_ENABLE = yes
 AUDIO_ENABLE ?= yes           # Audio output on port C6
 RGBARRAY_ENABLE ?= yes
+BACKLIGHT_ENABLE = no
 
 MCU = at90usb1286
 
